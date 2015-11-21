99f5e47241f070dd73e49eea352ca25bb3ff1b15
Maxim Bulatov <dvenum.at@gmail.com>
2015-11-22 04:31:12 +0600

    Corrected type in diffusion_toolkit/odf.py

--- nipype/interfaces/diffusion_toolkit/odf.py
+++ nipype/interfaces/diffusion_toolkit/odf.py
@@ -21,8 +21,8 @@ from ...utils.filemanip import fname_presuffix, split_filename, copyfile
 
 
 class HARDIMatInputSpec(CommandLineInputSpec):
-    bvecs = File(exists=True, desc='b vectors file',
-    bvals = File(exists=True,desc = 'b values file')
+    bvecs = File(exists=True,desc='b vectors file', argstr='%s', position=1)
+    bvals = File(exists=True,desc='b values file')
     gradient_table = File(exists=True,desc = 'Input gradient table', position=1, argstr='%s')
     out_file = File("recon_mat.dat", desc='output matrix file', argstr='%s', usedefault=True, position=2)
     order = traits.Int(argstr='-order %s', desc="""maximum order of spherical harmonics. must be even number. default
